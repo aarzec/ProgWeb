83e2713a5cb0c0687530664ef65a679dca7f1833
Andrés Romero <andresrz2k21@gmail.com>
2024-12-13 10:55:40 -0500

    Intro a JS

---
+++ P2/1 Scripting/first.js
@@ -0,0 +1,9 @@
+var nombre, edad, altura;
+
+nombre = "Juan";
+edad = 20;
+altura = 1.60;
+
+console.log("El nombre de la persona es: " + nombre);
+console.log("La edad de la persona es: " + edad);
+console.log("La altura de la persona es: " + altura);
